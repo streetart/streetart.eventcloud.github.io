7214538d18e1f4061d19385b987ba5b31ef08a32
streetart <overloadedpixel@gmail.com>
2018-07-24 00:10:18 +0200

    first push

---
+++ src/app/app.module.ts
@@ -0,0 +1,50 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {APP_CONFIG, AppConfig} from './app.config';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {AppComponent} from './app.component';
+import {WeatherListComponent} from './weather/weather-list.component';
+import {WeatherItemComponent} from './weather/weather-item.component';
+import {WeatherSearchComponent} from './weather/weather-search.component';
+import {WeatherService} from './weather/weather.service';
+import {HeaderComponent} from './header/header.component';
+import {PageNotFoundComponent} from './page-not-found.component';
+import {AppRoutingModule} from './app-routing.module';
+import {WeatherTodayComponent} from './weather-today/weather-today.component';
+
+
+
+
+// import {TranslateModule} from '@ngx-translate/core';
+
+@NgModule({
+
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+
+
+    ],
+
+    declarations: [
+        AppComponent,
+        WeatherSearchComponent,
+        WeatherListComponent,
+        WeatherItemComponent,
+        HeaderComponent,
+        PageNotFoundComponent,
+        WeatherTodayComponent,
+
+    ],
+
+    providers: [WeatherService, {provide: APP_CONFIG, useValue: AppConfig}],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
