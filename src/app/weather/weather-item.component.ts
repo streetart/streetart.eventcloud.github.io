8e829d17139ae26a1f62073b93a1c813f37ed375
streetart <overloadedpixel@gmail.com>
2018-07-25 01:16:30 +0200

    site structure

--- src/app/weather/weather-item.component.ts
+++ src/app/weather/weather-item.component.ts
@@ -4,11 +4,11 @@ import {Weather} from './weather';
 @Component({
     selector: 'weather-item',
     templateUrl: './weather-item.component.html'
-    //inputs: ['weatherItem']
+    // inputs: ['weatherItem']
 })
 export class WeatherItemComponent {
     @Input('WeatherItem') weather: Weather;
-    // constructor(){
-    //   this.weather = new Weather('Amritsar', 'Cloudy', 24);
-    // }
+    constructor(){
+      this.weather = new Weather('Amritsar', 'Cloudy', 24);
+    }
 }
--- src/app/weather/weather.ts
+++ src/app/weather/weather.ts
@@ -1,10 +1,9 @@
-export class Weather{
-   constructor(
-               public city: string, 
-               public description: string,
-               public temperature: number){
-                     this.city = city;
-                     this.description = description;
-                     this.temperature = temperature;
-               } 
- }
+export class Weather {
+    constructor(public city: string,
+                public description: string,
+                public temperature: number) {
+        this.city = city;
+        this.description = description;
+        this.temperature = temperature;
+    }
+}
